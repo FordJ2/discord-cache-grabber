5207a0e34cd27af405c75fd909bd265a7334e8aa
Jonathan <78560204+wncry@users.noreply.github.com>
2022-01-09 15:01:34 +0000

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,18 +1,18 @@
 @echo off
 
-#:here
+#:here          # start of loop
 
 cd %appdata%\discord\cache
-ren * *.png
-echo .
-echo please wait as images are converting	
-echo .
+ren * *.png     # renames the files
+echo ================
+echo convert complete
+echo ================
 timeout 5
-start %appdata%\discord\cache
+start %appdata%\discord\cache   # opens the dir
 
-#timeout 600
-#goto here
+#timeout 600    # in seconds
+#goto here      # repeat
 #end of loop
 
-#remove all comments (#) for the script to be automated every 10m (600s) 
+# remove all above comments (#) for the script to be automated to run every 10m (600s) 
 
