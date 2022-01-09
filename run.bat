79bd32af289f5cd76f0d26aecb1659b206157aeb
Jonathan <78560204+wncry@users.noreply.github.com>
2022-01-09 15:07:26 +0000

    fixed the comments bc i was dumb lmao

--- run.bat
+++ run.bat
@@ -1,18 +1,18 @@
 @echo off
 
-#:here          # start of loop
+:: :here          
 
 cd %appdata%\discord\cache
-ren * *.png     # renames the files
+ren * *.png
 echo ================
 echo convert complete
 echo ================
 timeout 5
-start %appdata%\discord\cache   # opens the dir
+start %appdata%\discord\cache
 
-#timeout 600    # in seconds
-#goto here      # repeat
-#end of loop
+:: timeout 600
+:: goto here
+:: end of loop
 
-# remove all above comments (#) for the script to be automated to run every 10m (600s) 
+rem remove all above comments (::) for the script to be automated to run every 10m (600s) 
 
